8f3e990f20886aaea93170d433df82bb67e32163
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:56:08 +0200

    Tomando snapshot de un ejercicio terminado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const style1 = {
+    fontSize: 30
+  }
+
+  const style2 = {
+    color: "#FF8C00", 
+    backgroundColor: "#FF2D00",
+    fontSize: "20px"
+  }
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+    style1.color = "#04756F"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+    style1.color = "#2E0927"
+  } else {
+    timeOfDay = "night"
+    style1.color = "#D90000"
+  }
+  
+  return (
+    <div>
+      <h1 style={style1}>Good {timeOfDay}!</h1>
+      <h1 style={style2}>Good {timeOfDay}!</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
